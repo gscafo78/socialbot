2fdb71ccf080905bc330e9c8df943d387effa455
Giovanni SCAFETTA <giovanni.scafetta@gmail.com>
2025-06-19 11:48:21 +0200

    feat: aggiornare Dockerfile e entrypoint.sh per migliorare la gestione delle dipendenze e semplificare la configurazione

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -6,11 +6,5 @@ if [ ! -f "/app/settings.json" ]; then
     cp settings_example.json settings.json
 fi
 
-# Check and copy feeds.json if missing
-if [ ! -f "/app/feeds.json" ]; then
-    echo "feeds.json not found. Copying from feeds_example.json..."
-    cp feeds_example.json feeds.json
-fi
-
 # Start the main application
 python /app/socialbot.py
